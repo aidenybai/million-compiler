043f62a9023d2d5a25832998d487110df58f2ea8
Aiden Bai <aiden.bai05@gmail.com>
2021-08-05 19:41:43 -0700

    build: output sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import { babel } from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
+import filesize from 'rollup-plugin-filesize';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
@@ -8,18 +10,27 @@ export default {
     {
       file: 'dist/index.js',
       format: 'esm',
+      sourcemap: true,
     },
     {
       file: 'dist/index.min.js',
       format: 'esm',
       plugins: [terser()],
+      sourcemap: true,
     },
   ],
   plugins: [
+    sourcemaps(),
     resolve(),
+    filesize({
+      showBrotliSize: true,
+      showGzippedSize: false,
+      showMinifiedSize: false,
+    }),
     babel({
       babelHelpers: 'bundled',
       presets: [
+        // '@babel/preset-env',
         [
           '@babel/preset-react',
           {
